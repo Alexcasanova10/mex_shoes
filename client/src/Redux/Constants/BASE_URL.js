2ceb4e19c7bf2ffe7121fa158a44b8327cfe0461
AlexCasa <casanovaalex61@gmail.com>
2024-11-22 02:12:21 -0600

    cambios de url constant en clinete

--- client/src/Redux/Constants/BASE_URL.js
+++ client/src/Redux/Constants/BASE_URL.js
@@ -1,6 +1,6 @@
 
 
 // export const BASE_URL = "https://node-react-shop-pearl.vercel.app"
-// export const BASE_URL = "https://mex-shoes.vercel.app"
+export const BASE_URL = "https://mex-shoes.vercel.app"
 
-export const BASE_URL = "http://localhost:9000"
+// export const BASE_URL = "http://localhost:9000"
