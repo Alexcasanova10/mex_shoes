ad1ad447a2ea0f16ff192d9dd9f00f1a020c5d43
AlexCasa <casanovaalex61@gmail.com>
2024-11-22 00:42:09 -0600

    cambio de filter sizes

--- client/src/pages/Client/ProductDetail.jsx
+++ client/src/pages/Client/ProductDetail.jsx
@@ -78,25 +78,43 @@ function ProductDetail() {
                       ))}
                   </div> */}
 
-                <div className="grid grid-cols-4 gap-2">
+                {/* <div className="grid grid-cols-4 gap-2">
                     {product.sizes
-                      .filter((size) => size.quantity > 0) // Filtrar tallas disponibles
+                      .filter((size) => size.quantity > 0) 
                       .map((size) => (
                         <button
-                          key={size.size} // Asegúrate de que "size.size" exista como identificador único
+                          key={size.size} 
                           onClick={() =>
                             setSelectedSize(selectedSize === size.size ? null : size.size)
-                          } // Permitir seleccionar y deseleccionar
+                          } 
                           className={`px-4 py-2 border rounded ${
                             selectedSize === size.size ? "bg-black text-white" : "bg-white text-gray-800"
                           } hover:bg-gray-200`}
                         >
-                          {size.size} {/* Mostrar la talla */}
+                          {size.size} 
                         </button>
                       ))}
+                </div> */}
+
+                {/* nuevo siez filter */}
+                <div className="grid grid-cols-4 gap-2">
+                    {product.sizes && Array.isArray(product.sizes) && 
+                      product.sizes
+                        .filter((size) => size.quantity > 0) // Filtrar tallas disponibles
+                        .map((size) => (
+                          <button
+                            key={size.size} // Asegúrate de que "size.size" exista como identificador único
+                            onClick={() =>
+                              setSelectedSize(selectedSize === size.size ? null : size.size)
+                            } // Permitir seleccionar y deseleccionar
+                            className={`px-4 py-2 border rounded ${
+                              selectedSize === size.size ? "bg-black text-white" : "bg-white text-gray-800"
+                            } hover:bg-gray-200`}
+                          >
+                            {size.size} {/* Mostrar la talla */}
+                          </button>
+                        ))}
                   </div>
-
-
                    
                   </div>
                   
